b91c1ed98a3a44b006d8c97d602cd80ffff0afcf
TML45 <tassio.melo45@gmail.com>
2021-11-24 11:52:25 -0300

    atualizacao de saldo na transferencia

--- src/main/java/com/bugsbuster/projectCaptainTech/service/TransferenciaService.java
+++ src/main/java/com/bugsbuster/projectCaptainTech/service/TransferenciaService.java
@@ -1,16 +1,20 @@
 package com.bugsbuster.projectCaptainTech.service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bugsbuster.projectCaptainTech.model.Conta;
 import com.bugsbuster.projectCaptainTech.model.Transferencia;
+import com.bugsbuster.projectCaptainTech.repository.ContaRepository;
 import com.bugsbuster.projectCaptainTech.repository.TransferenciaRepository;
 
 @Service
-public class TransferenciaService implements InterfaceTransferenciaService{
+public class TransferenciaService implements InterfaceTransferenciaService {
 	@Autowired
 	TransferenciaRepository tranRepo;
+
+	@Autowired
+	ContaRepository contaRepo;
 
 	@Override
 	public Iterable<Transferencia> obterTodos() {
@@ -19,17 +23,22 @@ public class TransferenciaService implements InterfaceTransferenciaService{
 
 	@Override
 	public Iterable<Transferencia> obterPorDestino(int id) {
-		return  this.tranRepo.findByDestino(id);
+		return this.tranRepo.findByDestino(id);
 	}
 
 	@Override
-	public Transferencia criarTransferencia(Transferencia tran){
-		try {
-		
-		}catch(Exception e) {
-			System.out.println(e.toString());
+	public Transferencia criarTransferencia(Transferencia tran) {
+		Conta origem = contaRepo.getById(tran.getContaOrigem().getId_conta());
+		Conta destino = contaRepo.getById(tran.getContaDestino().getId_conta());
+
+		if (origem.getCliente().getAtivo() && destino.getCliente().getAtivo()) {
+			if (tran.getValor() - origem.getSaldo() <= 200) {
+				origem.setSaldo(origem.getSaldo()-tran.getValor());
+				destino.setSaldo(destino.getSaldo()+tran.getValor());
+				return this.tranRepo.save(tran);
+			}
 		}
-		return this.tranRepo.save(tran);
+		return null;
 	}
 
 	@Override
